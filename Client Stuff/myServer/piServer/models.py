a8de4f001a8f8bc3355a091bb6bb3e479f64a7a7
apo <apo@urbancode.com>
2012-10-24 23:36:30 -0400

    remove the pyc files from the repo, add unique to user email addresses

--- Client Stuff/myServer/piServer/models.py
+++ Client Stuff/myServer/piServer/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=18)
     password = models.CharField(max_length=18)
-    email = models.EmailField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
     sQuestion = models.CharField(max_length=100)
     sAnswer = models.CharField(max_length=50)
     lastAddress = models.IPAddressField()
