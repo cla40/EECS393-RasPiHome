a313ab4c3796676ce4c6fe412ab6b8faf35e13a9
apo <apo@urbancode.com>
2012-10-25 14:08:46 -0400

    edited git ignore to ignore database and point to correct file paths, added the templates folder index.html now shows a list of users as an example

--- Client Stuff/myServer/piServer/views.py
+++ Client Stuff/myServer/piServer/views.py
@@ -1,4 +1,11 @@
+from django.template import Context, loader
+from piServer.models import User
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    user_list = User.objects.all()
+    t = loader.get_template('index.html')
+    c = Context({
+        'user_list': user_list,
+    })
+    return HttpResponse(t.render(c))
