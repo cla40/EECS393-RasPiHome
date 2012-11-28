8cc33e30719426e6007f4ccf8c44a1b1a9ddd403
Pat <nave_a_nater@yahoo.com>
2012-11-27 22:29:57 -0500

    Stuff
    Stuff

--- Server Stuff/Logger.py
+++ Server Stuff/Logger.py
@@ -1,7 +1,7 @@
-from C:\Users\Anthony\Documents\GitHub\EECS393-RasPiHome\Client Stuff\myServer\piServer\models.py import User
-from C:\Users\Anthony\Documents\GitHub\EECS393-RasPiHome\Client Stuff\myServer\piServer\models.py import Building
-from C:\Users\Anthony\Documents\GitHub\EECS393-RasPiHome\Client Stuff\myServer\piServer\models.py import Outlet
-from C:\Users\Anthony\Documents\GitHub\EECS393-RasPiHome\Client Stuff\myServer\piServer\models.py import Alarm
+from C:\Users\Cimara\Documents\GitHub\EECS393-RasPiHome\Client Stuff\myServer\piServer\models.py import User
+from C:\Users\Cimara\Documents\GitHub\EECS393-RasPiHome\Client Stuff\myServer\piServer\models.py import Building
+from C:\Users\Cimara\Documents\GitHub\EECS393-RasPiHome\Client Stuff\myServer\piServer\models.py import Outlet
+from C:\Users\Cimara\Documents\GitHub\EECS393-RasPiHome\Client Stuff\myServer\piServer\models.py import Alarm
 from django.db import models
 from datetime import datetime
 import sys
@@ -16,7 +16,7 @@ import sys
 
 class logger(bID):
     #location will just be directly on the Pi when we can do that.
-    location = "C:\Users\Anthony\Desktop\test.txt,r+" #this is the location I currently am using
+    location = "C:\Users\Cimara\Desktop\test.txt,r+" #this is the location I currently am using
      def logAlarm(aID, wasFlipped):
 		#note whether it was changed or not
         a = Alarm.objects.filter(alarmID=aID)
--- Server Stuff/handler.py
+++ Server Stuff/handler.py
@@ -1,11 +1,9 @@
-from C:\Users\Anthony\Documents\GitHub\EECS393-RasPiHome\Client Stuff\myServer\piServer\models.py import User
-from C:\Users\Anthony\Documents\GitHub\EECS393-RasPiHome\Client Stuff\myServer\piServer\models.py import Building
-from C:\Users\Anthony\Documents\GitHub\EECS393-RasPiHome\Client Stuff\myServer\piServer\models.py import Outlet
-from C:\Users\Anthony\Documents\GitHub\EECS393-RasPiHome\Client Stuff\myServer\piServer\models.py import Alarm
 from django.db import models
 from datetime import datetime
 from logger.py import log, logAlarm, logTimer, checkServerStatus
 import sys
+
+foo = imp.load_source('Models','C:\Users\Cimara\Documents\GitHub\EECS393-RasPiHome\Client Stuff\myServer\piServer\models.py') 
 
 class handler(bID):
 	building = Building.objects.get(pk=bID)
@@ -16,31 +14,32 @@ class handler(bID):
 		outlet.save()
 		#call hardware code to actually flip
 		#if actual flip was successful
+		print "YO STATE CHANGE"
 		log(user.lastAddress, user.username, "".join(["Outlet ", outlet.outletName, " was switched from ", "off" if outlet.state else "on", " to ", "on" if outlet.state else "off"]), 0)
 		#else
 		#log(user.lastAddress, user.username, "".join(["Outlet ", outlet.outletName, " was not switched due to a hardware error"]), 1)
 	def checkAlarms():
 		alarms = Alarm.objects.filter(buildingID=bID)
 		for alarm in alarms:
-			if alarm.startTime is None
+			if alarm.startTime == None:
 				checkTimer(alarm)
-			else
+			else:
 				checkAlarm(alarm)
 	def checkTimer(timer):
-		if datetime.datetime.now() >= timer.endTime
+		if datetime.datetime.now() >= timer.endTime:
 			flipState(timer.outletID)
 			logTimer(timer.pk)
 			#timer.delete()
 	def checkAlarm(alarm):
-		if datetime.datetime.now() >= alarm.endTime
+		if datetime.datetime.now() >= alarm.endTime:
 			outlet = Outlet.objects.get(pk=alarm.outletID)
-			if outlet.state is not desiredState
+			if outlet.state is not desiredState:
 				flipState(alarm.outletID)
 				logAlarm(alarm.pk, true)
-			else
+			else:
 				logAlarm(alarm.pk, false)
 			#alarm.delete()
 	def killServer():
-		if building.onlineState is False
+		if building.onlineState is False:
 			log(user.lastAddress, user.username, "The building associated with these alarms is no longer online... Shutting down", 0)
 			sys.exit("The building associated with these alarms is no longer online... Shutting down")
